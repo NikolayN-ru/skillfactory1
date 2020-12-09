9a47ff74080c407cd097a9f3796bfdffeb6ee6ca
NikolayN <Napadaylonikolay@gmail.com>
2020-12-09 23:34:55 +0600

    второй комит

--- 2.py
+++ 2.py
@@ -6,13 +6,7 @@ s = None
 
 def VivodIgrovovoPolya():
     global a
-    print(a[0] + '|' + a[1] + '|' + a[2])
-    print('-----')
-    print(a[3] + '|' + a[4] + '|' + a[5])
-    print('-----')
-    print(a[6] + '|' + a[7] + '|' + a[8])
 
-    print('*******************')
     print('-' * 13)
     for i in range(0, 9, 3):
         print('|', a[i] + ' | ' + a[i + 1] + ' | ' + a[i + 2], '|')
@@ -28,17 +22,33 @@ def polya():
     return d
 
 
-def vin(user):
+def vin(user, i):
     global a, d, s, play
-
+    if a[0] == a[1] == a[2] == i:
+        play = False
+    elif a[3] == a[4] == a[5] == i:
+        play = False
+    elif a[6] == a[6] == a[8] == i:
+        play = False
+    elif a[0] == a[3] == a[6] == i:
+        play = False
+    elif a[1] == a[4] == a[7] == i:
+        play = False
+    elif a[2] == a[5] == a[8] == i:
+        play = False
+    elif a[0] == a[4] == a[8] == i:
+        play = False
+    elif a[2] == a[4] == a[6] == i:
+        play = False
     if play == False:
+        VivodIgrovovoPolya()
         print('выйграл игрок _{}_!'.format(user))
         return play
 
 
 def vvodnubmer(x, y):
     while x not in y:
-        x = int(input('Нолик, введите корректное поле:'))
+        x = int(input('введите корректное поле:'))
     return x
 
 
@@ -47,11 +57,12 @@ def hod_O(f):
     polya()
     VivodIgrovovoPolya()
     print('свободные поля под номерами: {0}'.format(d))
-    f = int(input('игрок Нолик, введите номер поля:'))
+    f = int(input('игрок Нолик |O|, введите номер поля:'))
     # проверка на коректность игрового поля
-    f=vvodnubmer(f, d)
-    vin('нолик')
+    f = vvodnubmer(f, d)
+
     a[f] = 'O'
+    vin('нолик', i='O')
     if play == False:
         return print('________игра завершена')
     hod_X(s)
@@ -62,16 +73,17 @@ def hod_X(f):
     polya()
     VivodIgrovovoPolya()
     print('свободные поля под номерами: {0}'.format(d))
-    f = int(input('игрок Крестик, введите номер поля:'))
+    f = int(input('игрок Крестик |X|, введите номер поля:'))
     f = vvodnubmer(f, d)
-    vin('крестик')
+
     a[f] = 'X'
+    vin('крестик', i='X')
     if play == False:
         return print('________игра завершена')
     hod_O(s)
 
 
-j = str(input('выберете игрока Нолик - \'o\' или Крестик - \'x\''))
+j = str(input('выберете игрока Нолик -|o| или Крестик -|x|'))
 if j == 'o':
     hod_O(s)
 elif j == 'x':
